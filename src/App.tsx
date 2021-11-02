c59c709b9fb97771953557a58f12627fbda17bfd
yurimatos11 <yuri.cabello@hotmail.com>
2021-11-02 14:27:28 -0300

    add footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Routes from './Routes/Index';
 import Header from './Components/Header';
 import GlobalStyles from './Styles/global';
+import Footer from './Components/Footer';
 
 const App = () => (
     <>
       <Header />
       <Routes />
       <GlobalStyles />
+      <Footer />
     </>
 );
 
---
+++ src/Components/Footer/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './styles.scss';
+
+import Wave from '../../assets/img/wave.svg';
+
+const Footer: React.FC = () => {
+  return <>
+   <img className="footerWave" src={Wave} alt="Wave" />
+    <div className="footerBody">
+      <div className="footerContent">
+        <p>Portfolio2 V.1 Create by Yuri Matos</p>
+      </div>  
+    </div>
+  </>;
+}
+
+export default Footer;
